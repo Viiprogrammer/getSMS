ce652020e96859311d494d39b7f4af7872297f30
viiprogrammer <m_telega@mail.ru>
2024-02-22 02:44:42 +0300

    chore: update example

--- exmaples/example.js
+++ exmaples/example.js
@@ -1,4 +1,4 @@
-const { GetSMS, ServiceApiError, errors } = require('getsms')
+const { GetSMS, ServiceApiError, TimeoutError, errors } = require('getsms')
 
 const sms = new GetSMS({
   key: 'bc103fa02b63f986cd102a6d2f5c',
@@ -27,6 +27,10 @@ const sms = new GetSMS({
       await sms.setStatus(6, id) // Accept, end
     } else console.log('No money')
   } catch (error) {
+    if (error instanceof TimeoutError) {
+      console.log('Timeout reached')
+    }
+
     if (error instanceof ServiceApiError) {
       if (error.code === errors.BANNED) {
         console.log(`Banned! Time ${error.banTime}`)
